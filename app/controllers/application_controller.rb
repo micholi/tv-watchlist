a3188dfe19fe2f9aa41d42d1e4c641caae985b5f
Michelle Olivieri <michelleo72@gmail.com>
2018-05-22 20:42:36 +0000

    updates to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
-# Placeholder
-# Code Here
+# require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :secret_session, "supersecret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    # code here
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,1 @@
-# Placeholder
-# Code Here
+# code here
