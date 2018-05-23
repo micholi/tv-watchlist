c1803d1ca4457b1f1875d572cfaa840f4a4ab079
Michelle Olivieri <michelleo72@gmail.com>
2018-05-23 20:39:26 +0000

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,26 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+
+  end
+
   get '/signup' do
     # code here
     erb :'/users/signup'
+  end
+
+  post '/signup' do
+    
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
   end
 
 end
