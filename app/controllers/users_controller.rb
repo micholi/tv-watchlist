a8e1def9121ca0cdfd653d4caf695fbdcbb56840
Michelle Olivieri <michelleo72@gmail.com>
2018-05-23 18:00:46 +0000

    updated login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
-get '/login' do
-  # code here
-  erb :'/users/login'
-end
+  get '/login' do
+    # code here
+    erb :'/users/login'
+  end
 
-get '/signup' do
-  # code here
-  erb :'/users/signup'
-end
+  get '/signup' do
+    # code here
+    erb :'/users/signup'
+  end
 
 end
