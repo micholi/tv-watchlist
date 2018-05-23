5fcdbf9ab2df9cd17d3f75cdc33e4223b1a63277
Michelle Olivieri <michelleo72@gmail.com>
2018-05-23 13:37:08 +0000

    added controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
-# code here
+get '/login' do
+  # code here
+  erb :'/users/login'
+end
+
+get '/signup' do
+  # code here
+  erb :'/users/signup'
+end
 
 end
