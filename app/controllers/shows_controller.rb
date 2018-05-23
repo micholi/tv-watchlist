6e689b89d93dd9e8b8272f014d923ac42084422c
Michelle Olivieri <michelleo72@gmail.com>
2018-05-23 15:59:33 +0000

    updated shows controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -10,4 +10,14 @@ class ShowsController < ApplicationController
     erb :'/shows/new'
   end
 
+  post '/shows' do
+    # validation here
+    @show = Show.create(name: params[:name], genre: params[:genre], description: params[:description], air_date: params[:air_date])
+    network = Network.find_or_create_by(name: params[:network_name])
+    @show.network_id = network.id
+    # user code
+    @show.save
+    redirect "/shows/#{@show.id}"
+  end
+
 end
