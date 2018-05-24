3148d82fed101ec9979bc91480e14ed4449362b8
Michelle Olivieri <michelleo72@gmail.com>
2018-05-24 15:52:14 +0000

    fixed session_secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :secret_session, "supersecret"
+    set :session_secret, "supersecret"
   end
 
   get '/' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  use Rack::Flash
+
   get '/login' do
     if logged_in?
       redirect '/shows'
@@ -14,25 +16,37 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect '/shows'
     else
-      redirect '/login'
+      flash[:message] = "Error!"
+      redirect to "/login"
+    #erb :'/users/login'
     end
   end
 
   get '/signup' do
-    # code here
+
     erb :'/users/signup'
   end
 
   post '/signup' do
-    if !params[:email].empty? && !params[:username].empty? && !params[:password].empty?
+    if params[:email].empty? || params[:username].empty? || params[:password].empty?
+      flash[:message] = "Please fill out all fields to continue."
+      redirect to '/signup'
+      #redirect '/signup', flash[:message] = "Please fill out all fields to continue."
+    elsif User.find_by(email: params[:email])
+      flash[:message] = "Sorry, that email address is already in use."
+      redirect '/signup'
+    else
+
       @user = User.create(email: params[:email], username: params[:username], password: params[:password])
       session[:user_id] = @user.id
       # flash message?
       redirect '/shows'
-    else
-      redirect '/signup'
+
     end
   end
+
+
+
 
   get '/logout' do
     if logged_in?
