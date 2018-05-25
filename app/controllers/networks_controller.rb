8148fcbde925240ecfb6d649cd07d578075fbb59
Michelle Olivieri <michelleo72@gmail.com>
2018-05-25 18:01:01 +0000

    added code to networks controller

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,5 +1,27 @@
 class NetworksController
 
-# code here
+  get '/networks' do
+    erb :'/networks/index'
+  end
 
-end
+  get '/networks/new' do
+    erb :'/networks/new'
+  end
+
+  post '/networks' do
+
+  end
+
+  get '/networks/:slug' do
+
+  end
+
+  get '/networks/:slug/edit' do
+
+  end
+
+  patch '/networks/:slug' do
+
+  end
+
+  end
