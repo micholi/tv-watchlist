d1f736ee354faca8f9a38aeef033e87d6ceb8209
Michelle Olivieri <michelleo72@gmail.com>
2018-05-27 22:06:34 +0000

    shows controller refactoring

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,7 @@
 class ShowsController < ApplicationController
 
   get '/shows' do
+    binding.pry
     user_check
     @shows = Show.all.order(:name)
     erb :'/shows/index'
@@ -8,13 +9,7 @@ class ShowsController < ApplicationController
 
   get '/shows/new' do
     user_check
-  #  if logged_in?
-  #    @user = current_user
-      erb :'/shows/new'
-#    else
-#      flash[:message] = "You must log in to perform this action!"
-#      redirect '/login'
-#  end
+    erb :'/shows/new'
   end
 
   post '/shows' do
@@ -22,24 +17,17 @@ class ShowsController < ApplicationController
     if Show.find_by(name: params[:name])
       flash[:message] = "You may not add a show that already exists!"
       redirect '/shows'
-  #  if !Network.find_by(name: params[:network_name]) && params[:new_network].empty?
-  #    flash[:message] = "Please fill out all required fields!"
     elsif params[:name].empty? || params[:genre].empty? || (!Network.find_by(name: params[:network_name]) && params[:new_network].empty?) || params[:description].empty? || params[:air_date].empty?
       flash[:message] = "Please fill out all required fields!"
-      # correct redirect?
       redirect '/shows/new'
     else
       @show = Show.create(name: params[:name], genre: params[:genre], description: params[:description], air_date: params[:air_date])
-    #  if Network.find_by(name: params[:network_name])
-    #    network = Network.find_by(name: params[:network_name])
-    #  elsif !params[:new_network].empty? && !Network.find_by(name: params[:new_network])
-    #    network = Network.create(name: params[:new_network])
-    network = Network.find_by(name: params[:network_name])
-    if !@network && !params[:new_network].empty?
-      network = Network.create(name: params[:new_network])
+      network = Network.find_by(name: params[:network_name])
+      if !@network && !params[:new_network].empty?
+        network = Network.create(name: params[:new_network])
       end
       @show.network_id = network.id
-      @show.owner_id = current_user.id
+      @show.owner = current_user
       @show.users << current_user
       @show.save
       redirect "/shows/#{@show.slug}"
@@ -49,13 +37,11 @@ class ShowsController < ApplicationController
   get '/shows/:slug' do
     user_check
     @show = Show.find_by_slug(params[:slug])
-    #@user = current_user
     erb :'/shows/detail'
   end
 
   get '/shows/:slug/add' do
     user_check
-    #@user = current_user
     @show = Show.find_by_slug(params[:slug])
     @user.shows << @show
     redirect "/shows/#{@show.slug}"
@@ -78,29 +64,19 @@ class ShowsController < ApplicationController
   patch '/shows/:slug' do
     user_check
     @show = Show.find_by_slug(params[:slug])
-  #  if !@show
-  #    flash[:message] = "Show not found."
-  #    redirect '/shows'
-    if !Network.find_by(name: params[:network_name]) && params[:other_network].empty?
+    if params[:name].empty? || params[:genre].empty? || (!Network.find_by(name: params[:network_name]) && params[:new_network].empty?) || params[:description].empty? || params[:air_date].empty?
       flash[:message] = "Please fill out all required fields!"
-    elsif params[:name].empty? || params[:genre].empty? || params[:network_name].empty? || params[:description].empty? || params[:air_date].empty?
-      flash[:message] = "Please fill out all required fields!"
-
-    @show = Show.find_by_slug(params[:slug])
-    # validity check
-    @show.update(name: params[:name], genre: params[:genre], description: params[:description], air_date: params[:air_date])
-
-    if params[:other_network].empty?
+      redirect '/shows/:slug/edit'
+    else
+      @show.update(name: params[:name], genre: params[:genre], description: params[:description], air_date: params[:air_date])
       network = Network.find_by(name: params[:network_name])
-    elsif !params[:other_network].empty? && !Network.find_by(name: params[:other_network])
-      network = Network.create(name: params[:other_network])
-      # test this and add flash message?
+      if !@network && !params[:new_network].empty?
+        network = Network.create(name: params[:new_network])
+      end
+      @show.network_id = network.id
+      @show.save
+      redirect "/shows/#{@show.slug}"
     end
-  end
-
-    @show.network_id = network.id
-    @show.save
-    redirect "/shows/#{@show.slug}"
   end
 
   delete '/shows/:slug' do
@@ -108,12 +84,10 @@ class ShowsController < ApplicationController
     @show = Show.find_by_slug(params[:slug])
     if @show && @show.owner == current_user
       @show.delete
-    # else needed?
+      redirect '/shows'
     end
-redirect '/shows'
   end
 
-  #private methods?
-  # def required_fields_check
+
 
 end
