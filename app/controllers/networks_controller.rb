2fde89a1d1c76090e419061153adcbf4daed0d55
Michelle Olivieri <michelleo72@gmail.com>
2018-05-27 19:53:55 +0000

    added user check to networks controller

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,11 +1,13 @@
 class NetworksController < ApplicationController
 
   get '/networks' do
+    user_check
     @networks = Network.all.order(:name)
     erb :'/networks/index'
   end
 
   get '/networks/:slug' do
+    user_check
     @network = Network.find_by_slug(params[:slug])
     erb :'/networks/info'
   end
